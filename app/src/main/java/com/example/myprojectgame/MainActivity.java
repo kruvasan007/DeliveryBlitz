6f94c96fde99552602ce8d2fc00cd2012950435b
kruvasan007 <surkova-hova@mail.ru>
2022-04-03 01:59:16 +0700

    First Commit

--- app/src/main/java/com/example/myprojectgame/MainActivity.java
+++ app/src/main/java/com/example/myprojectgame/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends BaseActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
